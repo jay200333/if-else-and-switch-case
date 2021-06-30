22f182cf7eaf9db79f6f7792dcb973c6ebcb2454
jay200333 <83073900+jay200333@users.noreply.github.com>
2021-06-30 09:36:06 +0530

    Update switchcase.java

--- switchcase.java
+++ switchcase.java
@@ -1,17 +1,23 @@
-public class Main {
+public class switchcase {
 
    public static void main(String args[]){
-     int num=2;
-     switch(num+2)
-     {
-        case 1:
-	  System.out.println("Case1: Value is: "+num);
-	case 2:
-	  System.out.println("Case2: Value is: "+num);
-	case 3:
-	  System.out.println("Case3: Value is: "+num);
-        default:
-	  System.out.println("Default: Value is: "+num);
+      int i=2;
+      switch(i)
+      {
+	 case 1:
+	   System.out.println("Case1 ");
+	   break;
+	 case 2:
+	   System.out.println("Case2 ");
+	   break;
+	 case 3:
+	   System.out.println("Case3 ");
+	   break;
+	 case 4:
+           System.out.println("Case4 ");
+           break;
+	 default:
+	   System.out.println("Default ");
       }
    }
 }
